ad66bfaf91b47479e8d7447cd3f46bcab9b437e0
Qing Qing He <qingqinghe916@hotmail.com>
2023-08-18 11:12:26 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,9 +1,10 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 1;
 
 function setup_pScope(pScope){
+  //  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
@@ -12,9 +13,9 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  // var layer1 = new PLayer(faces);
+  // layer1.mode( SWIRL(5) );
+  // layer1.set_boundary( 200, 1000 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
@@ -40,10 +41,21 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  fill('#eae2b7');
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  fill('#ffb703');
+  // rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  ellipse(-10, -300,100,100);
+  fill(214, 91, 9);
+  // beginShape(); // flowerpot
+  // point(-10, -300);
+  // vertex(-10, -300-animation.wave()*50);
+  strokeWeight(5);
+point(-25, -320);
+// point(68, 19);
+// point(21, 17);
+// point(32, 91);
+strokeWeight(1);
 
 }
