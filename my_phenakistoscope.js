af2c66e70b987ecf2ad65e2261c3619195dfecc9
Qing Qing He <qingqinghe916@hotmail.com>
2023-08-26 13:26:45 +1200

    update

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,12 +1,13 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-    pScope.output_mode(OUTPUT_GIF(1000));
-  //  pScope.output_mode(ANIMATED_DISK);
+    // pScope.output_mode(OUTPUT_GIF(1000));
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("coin", "png");
 }
 
 function setup_layers(pScope){
@@ -23,11 +24,13 @@ function setup_layers(pScope){
 }
 
 function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
+  pScope.draw_image("coin" , x, y);
+  
+  // scale(animation.frame*2);
+
+  // ellipse(0,0,50,50); // draw head
+  // fill(30);
   // ellipse(-10,-10,10,10); //draw eye
   // ellipse(10,-10,10,10); // draw eye
   // arc(0,10,20,10,0,180); // draw mouth
