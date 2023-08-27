1f75a05c97a51c758085efae47771ca2392d6aed
Qing Qing He <qingqinghe916@hotmail.com>
2023-08-27 18:54:02 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -2,7 +2,7 @@ const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
     // pScope.output_mode(OUTPUT_GIF(1000));
-  pScope.output_mode( ANIMATED_DISK);
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.draw_slits(false);
@@ -17,6 +17,10 @@ function setup_layers(pScope){
 
   new PLayer(null, '#ff0054');  //lets us draw the whole circle background, ignoring the boundaries
 
+  var coin2 = new PLayer(coins);
+      coin2.mode( SWIRL(5) );
+      coin2.set_boundary( 70, 250 );
+ 
   var piggy = new PLayer(piggy_bank);
       piggy.mode( RING );
       piggy.set_boundary( 500, 800 );
@@ -28,25 +32,21 @@ function setup_layers(pScope){
    var coin1 = new PLayer(coin);
   //  layer1.mode( SWIRL(5) );
   // layer1.set_boundary( 200, 1000 );
-
-//   var coin2 = new Player(coins);
-//       coin2.mode( SWIRL(5) );
-//       coin2.set_boundary( 200, 800 );
-// }
-
-function faces(x, y, animation, pScope){
-
-  // pScope.draw_image("coin" , x, y);
-  
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
 }
+
+// function faces(x, y, animation, pScope){
+
+//   // pScope.draw_image("coin" , x, y);
+  
+//   scale(animation.frame*2);
+
+//   ellipse(0,0,50,50); // draw head
+//   fill(30);
+//   ellipse(-10,-10,10,10); //draw eye
+//   ellipse(10,-10,10,10); // draw eye
+//   arc(0,10,20,10,0,180); // draw mouth
+
+// }
 
 function lucky_cats(x, y, animation, pScope){
 
@@ -54,15 +54,17 @@ function lucky_cats(x, y, animation, pScope){
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
+   fill('#eae2b7');
+   arc(x,y,1000,1000,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
   scale(animation.wave(0.5));
    
-  //  fill('#eae2b7');
-  //  arc(x,y,1000,1000,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  
    pScope.draw_image("luckyCat" , 200, 400);
-  // fill('#ffb703');
-  // // // rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-  // ellipse(-10, -300-animation.wave()*50,40,40);
-
+   push()
+   rotate(90 * animation.frame)
+   let rectJump = 750 + (animation.wave(1) * 50)
+   rect(150, rectJump, 100,100)
+   pop()
 
 }
 
@@ -76,7 +78,9 @@ function piggy_bank(x,y, animation, pScope) {
 }
 
 function coins(x,y, animation, pScope){
-  scale(animation.frame*2);
+  scale(animation.frame);
   fill('#ffb703');
   ellipse(0,0,50,50);
+  fill('#ffffff');
+  rect(-10, -10, 20, 20);
 }
