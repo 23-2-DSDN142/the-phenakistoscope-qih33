a7bf82dc27cf2b707dfb0a05452325c530127867
Qing Qing He <qingqinghe916@hotmail.com>
2023-09-03 20:26:02 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,38 +3,35 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
     // pScope.output_mode(OUTPUT_GIF(1000));
   // pScope.output_mode(STATIC_FRAME);
-  pScope.output_mode( ANIMATED_DISK);
+  pScope.output_mode(ANIMATED_DISK);
   
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.draw_slits(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  // pScope.load_image("coin", "png");
-  // pScope.load_image("luckyCat", "png");
-  // pScope.load_image("pig", "png");
+  pScope.load_image("coin", "png");
+  pScope.load_image("luckyCat", "png");
+  pScope.load_image("pig", "png");
 }
 
 function setup_layers(pScope){
 
   new PLayer(null, '#ef233c');  //lets us draw the whole circle background, ignoring the boundaries
  
-  // var piggy = new PLayer(piggy_bank);
-  //     piggy.mode( RING );
-  //     piggy.set_boundary( 600, 1000 );
+  var piggy = new PLayer(piggy_bank);
+      piggy.mode( RING );
+      piggy.set_boundary( 600, 1000 );
 
+  
 
-  // var coin2 = new PLayer(coins);
-  //     coin2.mode( SWIRL(8) );
-  //     coin2.set_boundary( 250, 700 );
+  var coin2 = new PLayer(coins);
+      coin2.mode( SWIRL(8) );
+      coin2.set_boundary( 250, 700 );
 
-  // var cat = new PLayer(lucky_cats);
-  //     cat.mode( RING );
-  //     cat.set_boundary( 50, 200 );
-  var circleScale = new PLayer(circles);
-      circleScale.mode( SWIRL(5));
-      circleScale.set_boundary( 250, 700 );
-
+  var cat = new PLayer(lucky_cats);
+      cat.mode( RING );
+      cat.set_boundary( 50, 200 );
 
    var coin1 = new PLayer(coin);
   //  layer1.mode( SWIRL(5) );
@@ -54,14 +51,6 @@ function setup_layers(pScope){
 //   arc(0,10,20,10,0,180); // draw mouth
 
 // }
-
-function circles(x,y, animation, pScope){
-  scale(animation.frame*2);
-  ellipse(0,15,100,100);
-  ellipse(85, 15, 50, 50);
-  fill('#d90429');
-
-}
 
 function lucky_cats(x, y, animation, pScope){
 
